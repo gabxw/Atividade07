e625711708c1aff53040ab1c1af5d9af36772a39
Gabriel <gabriel.jmaciel13@gmail.com>
2025-02-09 20:42:33 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade07/exercicio08.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade07
+{
+    internal class exercício08
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
