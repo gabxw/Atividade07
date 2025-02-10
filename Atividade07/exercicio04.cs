3aa879722c6a3ba55bf8c45ad100b8cc0f67e05e
gabrieljmaciel1 <33903850-gabrieljmaciel1@users.noreply.replit.com>
2025-02-10 12:22:18 +0000

    User checkpoint: Fix: Implement basic password authentication and address build errors.  Changes the startup object and adds password verification logic.

--- Atividade07/exercicio04.cs
+++ Atividade07/exercicio04.cs
@@ -11,6 +11,27 @@ namespace Atividade07
     {
         static void Main(string[] args)
         {
+            
+            Console.Write("Digite a sua senha: ");
+            string senha = Console.ReadLine();
+            if (senha == "1234"){
+                Console.WriteLine("Acesso Permitido");
+                Console.delay(2000);
+                Console.Clear();
+                Console.WriteLine("======Area de acesso======");
+            }else{
+                int i = 0;
+                while(i < 3){
+                    Console.WriteLine("Senha incorreta");
+                    Console.Write("Digite a sua senha: ");
+                    senha = Console.ReadLine();
+                    i++;
+                }
+                Console.WriteLine("Acesso negado");
+                Console.delay(2000);
+                Environment.Exit(0); 
+                
+            }
         }
     }
 }
