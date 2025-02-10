6d919b96761753ba9d77953c5ff23713c20e99eb
gabrieljmaciel1 <33903850-gabrieljmaciel1@users.noreply.replit.com>
2025-02-10 13:18:41 +0000

    User checkpoint: Fix: Update main method to exercicio05 and handle potential null values in input.

--- Atividade07/exercicio05.cs
+++ Atividade07/exercicio05.cs
@@ -11,6 +11,31 @@ namespace Atividade07
     {
         static void Main(string[] args)
         {
+            
+            int[] num = new int[4];
+            Console.Write("Digite um número inteiro: ");
+            if (!int.TryParse(Console.ReadLine(), out num[0])){
+                Console.WriteLine("Caractere inválido");
+            }else{
+                if (num.All(x => x >= 0)){
+                    int i = 0;
+                    while(i < 4){
+                        Console.Write("Digite um número inteiro: ");
+                        num[i] = Convert.ToInt32(Console.ReadLine());
+                        i++;
+                    }
+                    int soma = 0;
+                    i = 0;
+                    while(i < 4){
+                        soma += num[i];
+                        i++;
+                    }
+                    Console.Write($"A soma dos números digitados: {soma}");
+                }else{
+                    Console.WriteLine("Número negativo");
+                    Environment.Exit(0);
+                }
+            }
         }
     }
 }
