bba4c645e0033f75dd1317db3a819a9abe73a0d0
Gabriel José Maciel <gabriel.jmaciel13@gmail.com>
2025-02-10 11:51:48 +0000

    Assistant checkpoint: Remover acentos dos nomes das classes
    Assistant generated file changes:
    - Atividade07/exercicio07.cs: Corrigir nome da classe
    - Atividade07/exercicio08.cs: Corrigir nome da classe
    - Atividade07/exercicio09.cs: Corrigir nome da classe
    - Atividade07/exercicio10.cs: Corrigir nome da classe
    - Atividade07/exercicio11.cs: Corrigir nome da classe
    
    ---
    
    User prompt:
    
    está me incomando bastante essas linhas vermelhas de erro no código, sendo que não há nada errado.

--- Atividade07/exercicio10.cs
+++ Atividade07/exercicio10.cs
@@ -1,4 +1,5 @@
-﻿using System;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Atividade07
 {
-    internal class exercício10
+    internal class exercicio10
     {
         static void Main(string[] args)
         {
