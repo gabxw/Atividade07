6534bbd3270730a2d2f1368ce9a9948dd9884d1d
Gabriel <gabriel.jmaciel13@gmail.com>
2025-02-10 00:02:10 -0300

    atualização do exercício 1 e 2

--- Atividade07/exercicio02.cs
+++ Atividade07/exercicio02.cs
@@ -10,6 +10,24 @@ namespace Atividade07
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Digite algum número");
+            int num;
+
+            if (!int.TryParse(Console.ReadLine(), out num))
+            {
+                Console.WriteLine("Caractere Invalido");
+            }
+            else {
+                int i = 0;
+                Console.WriteLine($"Tabuada até o 10 do número {num} ");
+
+                while (i <= 10)
+                {
+                    var soma = num * i;
+                    Console.WriteLine($"{i}x{num}={soma}");
+                    i++;
+                }
+            }
         }
     }
 }
